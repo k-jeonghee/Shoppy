633adce914a50481a6c266ea4b3b604aa0d9cb9d
JeongHee Kim <93242226+dlfeksgo@users.noreply.github.com>
2023-10-04 23:04:14 +0900

    Cloudinary 이미지 파일 업로드

---
+++ src/api/uploader.js
@@ -0,0 +1,11 @@
+export const uploadImage = async (file) => {
+	const data = new FormData();
+	data.append('file', file);
+	data.append('upload_preset', process.env.REACT_APP_CLOUDINARY_PRESET);
+
+	const result = await fetch(process.env.REACT_APP_CLOUDINARY_URL, {
+		method: 'POST',
+		body: data,
+	});
+	return result;
+};
--- src/pages/NewProduct.jsx
+++ src/pages/NewProduct.jsx
@@ -1,7 +1,75 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { uploadImage } from '../api/uploader';
 
 const NewProduct = () => {
-	return <div>NewProduct</div>;
+	const [product, setProduct] = useState({});
+	const [file, setFile] = useState();
+	const handleChange = (e) => {
+		const { name, value, files } = e.target;
+		if (name === 'file') {
+			setFile(files && files[0]);
+			return;
+		}
+		setProduct((product) => ({
+			...product,
+			[name]: value,
+		}));
+	};
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		uploadImage(file).then((res) => console.log(res));
+	};
+
+	return (
+		<div className="flex flex-col items-center">
+			<h1 className="text-2xl font-bold">새로운 제품 등록</h1>
+			{file && <img src={URL.createObjectURL(file)} alt="미리보기" />}
+			<form className="flex flex-col w-3/4" onSubmit={handleSubmit}>
+				<input
+					className="p-4 border border-grey mb-2"
+					type="file"
+					name="file"
+					accept="/image/*"
+					required
+					onChange={handleChange}
+				/>
+				<input
+					className="p-4 border border-grey mb-2"
+					type="text"
+					placeholder="제품명"
+					name="title"
+					onChange={handleChange}
+					value={product.title || ''}
+				/>
+				<input
+					className="p-4 border border-grey mb-2"
+					type="text"
+					placeholder="가격"
+					name="price"
+					onChange={handleChange}
+					value={product.price || ''}
+				/>
+				<input
+					className="p-4 border border-grey mb-2"
+					type="text"
+					placeholder="카테고리"
+				/>
+				<input
+					className="p-4 border border-grey mb-2"
+					type="text"
+					placeholder="제품 설명"
+				/>
+				<input
+					className="p-4
+					border border-grey
+					mb-2"
+					type="text"
+					placeholder="옵션들(콤마(,)로 구분)"
+				/>
+				<button>제품등록</button>
+			</form>
+		</div>
+	);
 };
 
 export default NewProduct;
